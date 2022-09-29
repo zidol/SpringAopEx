5702000d51d95ad1555e9c0e03323b8d59605767
zidol <ajh3166@naver.com>
2022-09-29 22:03:28 +0900

    스프링 AOP 구현 - 포인트 컷 분리, 어드바이스 추가

--- src/main/java/hello/aop/AopApplication.java
+++ src/main/java/hello/aop/AopApplication.java
@@ -1,9 +1,18 @@
 package hello.aop;
 
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
+import hello.aop.order.aop.AspectV3;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Slf4j
 @SpringBootApplication
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+@Import(AspectV3.class)
 public class AopApplication {
 
 	public static void main(String[] args) {

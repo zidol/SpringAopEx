0b8f1681c82ce17d0e65876ec18444db3c211d4e
zidol <ajh3166@naver.com>
2022-10-02 21:38:22 +0900

    AOP 포인트 컷 참조 및 어드바이스 순서

--- src/main/java/hello/aop/AopApplication.java
+++ src/main/java/hello/aop/AopApplication.java
@@ -3,6 +3,7 @@ package hello.aop;
 import hello.aop.order.aop.AspectV1;
 import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
+import hello.aop.order.aop.AspectV4Pointcut;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +11,6 @@ import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootApplication
-//@Import(AspectV1.class)
-//@Import(AspectV2.class)
-@Import(AspectV3.class)
 public class AopApplication {
 
 	public static void main(String[] args) {
--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -2,15 +2,23 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV4Pointcut;
+import hello.aop.order.aop.AspectV5Order;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
 public class AopTest {
 
     @Autowired
